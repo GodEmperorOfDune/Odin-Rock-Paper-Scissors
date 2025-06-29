3d1b9536ffa62490bcd20488b9ce241c4cc356c3
Mark Tinker <mark.tinker.94@gmail.com>
2025-06-29 17:49:12 -0400

    added function to play single round

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,9 +1,11 @@
+// Rock = 0 Paper = 1 Scissors = 2
+const choices = {0: "Rock", 1: "Paper", 2: "Scissors"};
+let humanScore = 0;
+let computerScore = 0;
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
-
-// Rock = 0 Paper = 1 Scissors = 2
-const choices = {0: "Rock", 1: "Paper", 2: "Scissors"};
 
 function getComputerChoice () {
     return choices[getRandomInt(3)];
@@ -15,6 +17,38 @@ function getPlayerChoice () {
     return playerChoice;
 }
 
+function playRound (humanChoice, computerChoice) {
+    console.log(humanChoice);
+    console.log(computerChoice);
 
-console.log(getComputerChoice());
-console.log(getPlayerChoice());
+    if(humanChoice === computerChoice) {
+        console.log("It is a Tie: " + humanChoice + " is the same as " + computerChoice);
+        humanScore++;
+    }
+    if((humanChoice === "Rock") && (computerChoice === "Scissors")) {
+        console.log("You Win! " + humanChoice + " beats " + computerChoice);
+        humanScore++
+    }
+    if((humanChoice === "Paper") && (computerChoice === "Rock")) {
+        console.log("You Win! " + humanChoice + " beats " + computerChoice);
+        humanScore++
+    }
+    if((humanChoice === "Scissors") && (computerChoice === "Paper")) {
+        console.log("You Win! " + humanChoice + " beats " + computerChoice);
+        humanScore++
+    }
+    if((computerChoice === "Rock") && (humanChoice === "Scissors")) {
+        console.log("You Lose! " + humanChoice + " loses to " + computerChoice);
+        computerScore++;
+    }
+    if((computerChoice === "Paper") && (humanChoice === "Rock")) {
+        console.log("You Lose! " + humanChoice + " loses to " + computerChoice);
+        computerScore++;
+    }
+    if((computerChoice === "Scissors") && (humanChoice === "Paper")) {
+        console.log("You Lose! " + humanChoice + " loses to " + computerChoice);
+        computerScore++;
+    }
+}
+
+playRound(getPlayerChoice(), getComputerChoice());
