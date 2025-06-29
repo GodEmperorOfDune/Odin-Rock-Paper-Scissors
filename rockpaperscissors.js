c8328d70b552f7df25952de80f4102327014df0a
Mark Tinker <mark.tinker.94@gmail.com>
2025-06-29 17:23:57 -0400

    created function to generate computer choice

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -2,4 +2,11 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-console.log(getRandomInt(3));
+// Rock = 0 Paper = 1 Scissors = 2
+const choices = {0: "Rock", 1: "Paper", 2: "Scissors"};
+
+function getComputerChoice () {
+    return choices[getRandomInt(3)];
+}
+
+console.log(getComputerChoice());
